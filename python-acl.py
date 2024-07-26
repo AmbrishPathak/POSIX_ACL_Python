891a49b19537503d521cf8ab0392aaad913b599a
Ambrish Pathak <31730377+AmbrishPathak@users.noreply.github.com>
2024-07-26 12:37:39 +0200

    Update python-acl.py

--- python-acl.py
+++ python-acl.py
@@ -1,1 +1,17 @@
+import os
 
+from posix import aclexec
+
+def main():
+
+#Get the absololute path to the script.
+
+script_path = os.patha.abspath(__file__)
+
+# Set the owner to read, write, and execute the script 
+
+os.chmod(script_path, 0755)
+
+# Set the group to read and execute the script 
+
+os.chmod(Script_path, 0550)
